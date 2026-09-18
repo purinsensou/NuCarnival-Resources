dbe458751f25bb252fa6bdf80dd7fbb5397e4c40
Andrei Marcelino <144007152+purinsensou@users.noreply.github.com>
2026-09-18 20:08:30 +0200

    Update app metadata for Vercel App

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Vercel App',
+  description: 'Resources and calculators app',
+  generator: ' ',
   icons: {
     icon: [
       {
